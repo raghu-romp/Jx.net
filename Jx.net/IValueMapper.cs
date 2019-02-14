b8123016ee23771402151765d7482a8b2f1134a5
Raghu Romp <raghu.romp@hotmail.com>
2019-02-14 21:45:57 +1100

    Initial project setup + basic transformation completed

---
+++ Jx.net/IValueMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jx.net
+{
+    public interface IValueMapper<FT, TT>
+    {
+        IDictionary<FT,TT> ValueMapping { get; }
+        TT Map(FT value);
+    }
+
+    public interface IValueMapper<T> : IValueMapper<T, T>
+    {
+    }
+}
---
+++ Jx.net/JsonMapper.cs
@@ -0,0 +1,16 @@
+﻿using JUST;
+
+namespace Jx.net
+{
+    public class JsonMapper : IJsonMapper
+    {
+        public JsonMapper()
+        {
+        }
+
+        public string Map(string sourceJson, string mapperJson)
+        {
+            return JsonTransformer.Transform(mapperJson, sourceJson);
+        }
+    }
+}
