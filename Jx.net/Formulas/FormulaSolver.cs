fe363e3fffb81bc85ed6f6e1e0bb4bb95d3f159d
Raghu Romp <raghu.romp@hotmail.com>
2019-04-20 07:49:45 +1000

    Formula support initial work

---
+++ Jx.net/Formulas/FormulaSolver.cs
@@ -0,0 +1,29 @@
+﻿using Flee.PublicTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jx.net.Formulas
+{
+    public class FormulaSolver : IFormulaSolver
+    {
+        private static ExpressionContext context = new ExpressionContext();
+
+        static FormulaSolver() {
+            context = CreateExpressionContext();
+        }
+
+        public T Solve<T>(string expression, Dictionary<string, dynamic> variables) {
+            throw new NotImplementedException();  
+        }
+
+        private static ExpressionContext CreateExpressionContext() {
+            var context = new ExpressionContext();
+            context.Imports.AddType(typeof(Math));
+
+            //this.additionalTypesToRegister.ForEach(type => context.Imports.AddType(type.Value, type.Name));
+
+            return context;
+        }
+    }
+}
