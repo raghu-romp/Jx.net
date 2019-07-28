9fbebd09e3f54b6979a8889a7f34ab19f476e906
Raghavendra Rompicharala <raghu.romp@hotmail.com>
2019-07-28 14:53:45 +1000

    Added additional built-in pipes

---
+++ Jx.net/ValueMap/BuildIn/StringCasing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jx.net.ValueMap.BuildIn
+{
+    public class ToUpper : IValuePipe
+    {
+        public string Name => "ToUpper";
+
+        public dynamic Process(dynamic fromValue) {
+
+            if (!fromValue is string) {
+                throw new ArgumentException($"{nameof(fromValue)} should be a string value");
+            }
+
+            return ((string)fromValue).ToUpper();
+        }
+    }
+
+    public class ToLower : IValuePipe
+    {
+        public string Name => "ToLower";
+
+        public dynamic Process(dynamic fromValue) {
+
+            if (!fromValue is string) {
+                throw new ArgumentException($"{nameof(fromValue)} should be a string value");
+            }
+
+            return ((string)fromValue).ToLower();
+        }
+    }
+}
---
+++ Jx.net/ValueMap/BuildIn/TypeConvert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jx.net.ValueMap.BuildIn
+{
+    public class ToString : IValuePipe
+    {
+        public string Name => "ToString";
+
+        public dynamic Process(dynamic fromValue) {
+            return fromValue == null ?
+                string.Empty : fromValue.ToString();
+        }
+    }
+}
